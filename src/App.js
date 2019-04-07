8ca54de85ae6b1defb84458635dde85c262b4925
CurbYourStrangeness <devbuild2018@gmail.com>
2019-04-06 23:44:41 -0500

    began with constructors in src/App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  constructor(){
+   
+  }
+
   render() {
     return (
       <div className="App">
